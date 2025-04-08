3c03339fc37a0c84d9c5d4e63b9df989b80ea268
aratik17 <aratikundargi17@gmail.com>
2025-04-09 04:16:11 +0530

    save

---
+++ JAVA/datatype.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class datatype{
+    public static void main(String[] args){
+        char a='c';
+        int i=89;
+        byte b=4;
+        short s=56;
+        double d=4.768593920;
+        float f=4.845f;
+        long l=8748786;
+        System.out.println("char:"+a);
+        System.out.println("int:"+i);
+        System.out.println("byte:"+b);
+        System.out.println("short:"+s);
+        System.out.println("double:"+d);
+        System.out.println("float:"+f);
+        System.out.println("long:"+l);
+    }
+}
